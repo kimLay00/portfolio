144181a09594f28a4f92336b65204132fc5e213a
H199 <199@tranthinktech.com>
2022-12-15 00:48:51 +0800

    dev: first commit of portfolio template implementation

---
+++ src/views/Resume/Components/EducationLicences.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-flex xs12 sm12 md5 lg6 xl6 class="educationLiencesContainer mt-4 pt-4">
+      <h2 class="mb-4 pl-4 pt-2">Education/ Licences</h2>
+      <v-card-text class="py-0">
+        <v-timeline align-top dense>
+
+          <v-timeline-item v-for="($timeLineItem, $index) in timeLineItems" :key="`timeLineItem${$index}`" color="light-blue" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold light-blue--text text--darken-1">{{$timeLineItem.startDate}} -</div>
+                <div class="caption font-weight-bold light-blue--text text--darken-1">{{$timeLineItem.endDate}}</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>{{$timeLineItem.title}}</strong>
+                <div class="caption mb-2">{{$timeLineItem.institute}}</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+        </v-timeline>
+      </v-card-text>
+    </v-flex>
+</template>
+<script>
+    export default {
+        name: "EducationLicences",
+    data() {
+        return {
+        timeLineItems:[
+            {startDate: "JAN 2022", endDate: "PRESENT", title: "Project Management Profession (PMP)", institute: "Project Management Institute"},
+            {startDate: "SEP 2013", endDate: "MAY 2017", title: "Bachelor of Engineering in Information Engineering", institute: "The Chinese University of Hong Kong"},
+        ],
+        };
+    },
+    mounted() {
+    },
+
+    beforeDestroy() {
+    },
+    };
+</script>
+<style >
+</style>
---
+++ src/views/Resume/Resume.vue
@@ -0,0 +1,133 @@
+<template>
+  <v-layout row justify-center align-center wrap class="mt-4 pt-2">
+    <EducationLicences></EducationLicences>
+    <Experience></Experience>
+    <v-layout row justify-center align-center wrap class="mt-4 pt-2">
+      <v-flex xs12 sm12 md4 lg5 xl5 class="mx-2 pt-4">
+        <h2>
+          <span>Frontend</span>
+
+          <span class="light-blue--text text--darken-1">Language</span>
+        </h2>
+        <br>
+        <div v-for="($skillItem, $index) in frontendSkills" :key="`frontend${$index}Skills`">
+          <strong>{{$skillItem.skills}}</strong>
+          <v-progress-linear v-model="$skillItem.barValue" :buffer-value="bufferValue" color="light-blue" height="16"></v-progress-linear>
+        </div>
+      </v-flex>
+      <v-flex xs12 sm12 md4 lg5 xl5 class="mx-2 pt-4">
+        <h2>
+          <span>Version</span>
+
+          <span class="light-blue--text text--darken-1">Control</span>
+        </h2>
+        <br>
+        <div v-for="($skillItem, $index) in versionControlSkills" :key="`versionControl${$index}Skills`">
+          <strong>{{$skillItem.skills}}</strong>
+          <v-progress-linear v-model="$skillItem.barValue" :buffer-value="bufferValue" color="light-blue" height="16"></v-progress-linear>
+        </div>
+      </v-flex>
+      <div class="mt-4 pt-2">
+        <v-btn
+          href="https://docs.google.com/document/d/1WSR42FEgUahJuR_RArW1wz7TfUBlgj13zekRidmQgs4/edit?usp=sharing"
+          target="_blank"
+          color="light-blue"
+          dark
+          large
+        >
+          Download Resume
+          <v-icon right dark>fas fa-file-download</v-icon>
+        </v-btn>
+      </div>
+    </v-layout>
+  </v-layout>
+</template>
+
+<script>
+import EducationLicences from "./Components/EducationLicences";
+import Experience from "./Components/Experience";
+
+export default {
+  metaInfo: {
+    title: "Resume",
+    titleTemplate: "%s ← Eldin's Space",
+    meta: [
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        name: "description",
+        content:
+          "Vue NUXT HTML CSS JavaScript Design Adobe XD Photoshop Coding Skills Resume Work Web Developer Front-End"
+      },
+      { charset: "utf-8" },
+      { property: "og:title", content: "Eldin' Space" },
+      { property: "og:site_name", content: "Eldin' Space" },
+      { property: "og:type", content: "website" },
+      { property: "og:url", content: "https://eldin.space" },
+      {
+        property: "og:image",
+        content: "https://i.imgur.com/Dcz2PGx.jpg"
+      },
+      {
+        property: "og:description",
+        content:
+          "Vue NUXT HTML CSS JavaScript Design Adobe XD Photoshop Coding Skills Resume Work Web Developer Front-End"
+      }
+    ]
+  },
+  components:{
+    EducationLicences,
+    Experience
+  },
+  data() {
+    return {
+      value: 0,
+      frontendSkills:[
+        {skills: "Vue", barValue: 0},
+        {skills: "Javascript", barValue: 0},
+        {skills: "Typescript", barValue: 0},
+        {skills: "HTML", barValue: 0},
+        {skills: "CSS/ SCSS", barValue: 0},
+        {skills: "JQuery", barValue: 0},
+        {skills: "Angular", barValue: 0},
+      ],
+      versionControlSkills:[
+        {skills: "GIT", barValue: 0},
+        {skills: "SVN", barValue: 0},
+      ],
+      bufferValue: 100,
+      interval: 0
+    };
+  },
+  mounted() {
+    this.startBuffer();
+  },
+
+  beforeDestroy() {
+    clearInterval(this.interval);
+  },
+
+  methods: {
+    startBuffer() {
+      this.interval = setInterval(() => { 
+        console.warn("hihi");
+          this.frontendSkills = [
+            {skills: "Javascript", barValue: 90},
+            {skills: "Vue", barValue: 80},
+            {skills: "HTML", barValue: 80},
+            {skills: "CSS/ SCSS", barValue: 80},
+            {skills: "JQuery", barValue: 80},
+            {skills: "Typescript", barValue: 65},
+            {skills: "Angular", barValue: 60},
+          ];
+          this.versionControlSkills = [
+            {skills: "GIT", barValue: 90},
+            {skills: "SVN", barValue: 60},
+          ];
+        }, 800);
+    }
+  }
+};
+</script>
+<style >
+</style>
+
