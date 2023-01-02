e8a112c56b01e1a6d7f9002d6862a6b1cd728640
H199 <199@tranthinktech.com>
2023-01-02 22:58:03 +0800

    dev: [Contact] Update Contact info

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,169 +1,38 @@
 <template>
   <v-container grid-list-xl>
-    <v-layout row justify-center align-center wrap class="mt-4 pt-2">
+    <v-layout row justify-start align-center wrap class="mt-4 pt-2">
       <v-flex xs12 sm12 md6 lg6 xl6>
         <h2 class="pb-4 mt-2">
           <span>GetIn</span>
           <span class="light-blue--text text--darken-1">Touch</span>
         </h2>
         <div class="py-4 subheading font-weight-bold">
-          <v-icon large color="green" left>fas fa-map-marker-alt</v-icon>
-          <span>Doboj,&nbsp;</span>
-          <span class="light-blue--text text--darken-1">Bosnia & Herzegovina</span>
+          <v-icon large color="light-blue" left>fas fa-map-marker-alt</v-icon>
+          <span>Hong&nbsp;</span>
+          <span class="light-blue--text text--darken-1">Kong</span>
         </div>
         <div class="py-4 subheading font-weight-bold">
-          <v-icon large color="green" left>fas fa-envelope</v-icon>
-          <span>eldin@</span>
-          <span class="light-blue--text text--darken-1">zaimovic.com</span>
+          <v-icon large color="light-blue" left>fas fa-envelope</v-icon>
+          <span>mskimberlylay@</span>
+          <span class="light-blue--text text--darken-1">gmail.com</span>
         </div>
         <div class="py-4 subheading font-weight-bold">
-          <v-icon large color="green" left>fas fa-phone</v-icon>
-          <span>+387&nbsp;</span>
-          <span class="light-blue--text text--darken-1">61 596 676</span>
-        </div>
-        <div class="py-4 subheading font-weight-bold">
-          <v-icon large color="green" left>fas fa-check</v-icon>
-          <span>Freelance</span>
-          <span class="light-blue--text text--darken-1">Available</span>
+          <v-icon large color="light-blue" left>fas fa-phone</v-icon>
+          <span>+852&nbsp;</span>
+          <span class="light-blue--text text--darken-1">91527629</span>
         </div>
       </v-flex>
 
-      <v-flex xs12 sm12 md6 lg6 xl6>
-        <h2 class="pb-4 mb-4">
-          <span>Contact</span>
-          <span class="light-blue--text text--darken-1">Form</span>
-        </h2>
-
-        <form method="POST" action="https://formspree.io/eldin@zaimovic.com">
-          <v-text-field
-            name="name"
-            color="green"
-            background-color="transparent"
-            v-model="name"
-            :error-messages="nameErrors"
-            label="Name"
-            required
-            @blur="$v.name.$touch()"
-          ></v-text-field>
-          <v-text-field
-            type="email"
-            color="green"
-            background-color="transparent"
-            name="email"
-            v-model="email"
-            :error-messages="emailErrors"
-            label="E-mail"
-            required
-            @blur="$v.email.$touch()"
-          ></v-text-field>
-          <v-textarea
-            color="green"
-            background-color="transparent"
-            :counter="200"
-            :error-messages="bodyErrors"
-            v-model="body"
-            label="Textarea"
-            name="body"
-            @blur="$v.body.$touch()"
-          ></v-textarea>
-          <v-btn
-            @click="submit"
-            type="submit"
-            color="green"
-            class="white--text"
-            :disabled=" (body.length<=20)"
-          >SEND MESSAGE</v-btn>
-          <v-btn @click="clear">clear</v-btn>
-        </form>
-      </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-import { validationMixin } from "vuelidate";
-import {
-  required,
-  maxLength,
-  email,
-  minLength
-} from "vuelidate/lib/validators";
 export default {
-  metaInfo: {
-    title: "Contact",
-    titleTemplate: "%s ← Eldin's Space",
-    meta: [
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
-        name: "description",
-        content:
-          "Eldin Zaimovic's Contact Doboj Bosnia and Herzegovina Freelance Get in Touch ContactMe"
-      },
-      { charset: "utf-8" },
-      { property: "og:title", content: "Eldin' Space" },
-      { property: "og:site_name", content: "Eldin' Space" },
-      { property: "og:type", content: "website" },
-      { property: "og:url", content: "https://eldin.space" },
-      {
-        property: "og:image",
-        content: "https://i.imgur.com/Dcz2PGx.jpg"
-      },
-      {
-        property: "og:description",
-        content:
-          "Eldin Zaimovic's Contact Doboj Bosnia and Herzegovina Freelance Get in Touch ContactMe"
-      }
-    ]
-  },
-  mixins: [validationMixin],
-  validations: {
-    name: { required, maxLength: maxLength(20) },
-    email: { required, email },
-    body: { required, minLength: minLength(20) }
-  },
   data() {
     return {
-      name: "",
-      email: "",
-      body: ""
     };
   },
-  methods: {
-    submit() {
-      this.$v.$touch();
-    },
-    clear() {
-      this.$v.$reset();
-      this.name = "";
-      this.email = "";
-      this.body = "";
-    }
-  },
-  computed: {
-    nameErrors() {
-      const errors = [];
-      if (!this.$v.name.$dirty) return errors;
-      !this.$v.name.maxLength &&
-        errors.push("Name must be at most 20 characters long");
-      !this.$v.name.required && errors.push("Name is required.");
-      return errors;
-    },
-    emailErrors() {
-      const errors = [];
-      if (!this.$v.email.$dirty) return errors;
-      !this.$v.email.email && errors.push("Must be valid e-mail");
-      !this.$v.email.required && errors.push("E-mail is required");
-      return errors;
-    },
-    bodyErrors() {
-      const errors = [];
-      if (!this.$v.body.$dirty) return errors;
-      !this.$v.body.minLength &&
-        errors.push("Text must be at least 20 characters long");
-      !this.$v.body.required && errors.push("Text is required");
-      return errors;
-    }
-  }
 };
 </script>
 
